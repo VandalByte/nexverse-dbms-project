a83e041dd05d41152d4d135bd00d1c8b7a71fbfc
Vandal <86826719+VandalByte@users.noreply.github.com>
2024-04-07 22:34:01 +0530

    Add files via upload

---
+++ export.py
@@ -0,0 +1,9 @@
+
+# sample data output format
+#  for table 'account'
+# "user_id int(10), username varchar(20), password varchar(64), email varchar(30), join_date date, last_login date, is_private int(1)",
+
+
+data = [
+    [1001, "ameliaxx", "54fcf974eabb0444320acd2835977b2c686b916162e6571668ac45db549da031", "amelia34@gmail.com", "2024-05-15", "2024-08-16 22:43", 0]
+]
