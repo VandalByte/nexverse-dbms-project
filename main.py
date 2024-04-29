154d7eb8504949156f66cf50effc8658c35cdea0
Vandal <86826719+VandalByte@users.noreply.github.com>
2024-04-29 12:17:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import re
 import os
 import hashlib
 import shutil
+import pandas as pd
+# import matplotlib
+import xlsxwriter
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
 import mysql.connector as sql
 
-
-# credentials
-HOST = "localhost"
-USER = "root"
-PASSWD = "root"
-DATABASE = "fun"
-ADMIN_PASSWD = "123"
+# user defined modules
+from utils import *
 
 # ANSI colors
 RED = "\033[91m"
@@ -97,8 +97,9 @@ def check_table_exists(cursor: Any) -> None:
     db_tables = {
         "account": "user_id INT(10) AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20), password VARCHAR(64), email VARCHAR(30), join_date VARCHAR(10), last_login VARCHAR(16), is_private VARCHAR(1)",
         "user": "username VARCHAR(20) PRIMARY KEY, fname VARCHAR(20), lname VARCHAR(20), bio VARCHAR(120), location VARCHAR(100), age INT(3)",
-        "post": "post_id INT(10) AUTO_INCREMENT PRIMARY KEY, user_id VARCHAR(10), content VARCHAR(500), timestamp VARCHAR(16), comments VARCHAR(10), likes VARCHAR(10)",
-        "comment": "comment_id INT(10) AUTO_INCREMENT PRIMARY KEY, post_id VARCHAR(10), content VARCHAR(100), timestamp VARCHAR(16), likes VARCHAR(10)",
+        "post": "post_id INT(10) AUTO_INCREMENT PRIMARY KEY, user_id INT(10), content VARCHAR(500), category VARCHAR(20), timestamp VARCHAR(16)",
+        "reaction": "reaction_id INT(10) AUTO_INCREMENT PRIMARY KEY, post_id INT(10), user_id INT(10), reaction_score INT(1), timestamp VARCHAR(16)",
+        #"comment": "comment_id INT(10) AUTO_INCREMENT PRIMARY KEY, post_id INT(10), content VARCHAR(100), timestamp VARCHAR(16), likes VARCHAR(10)",
     }
 
     # iterate over the dictionary and check if each table exists
@@ -170,7 +171,7 @@ def signup(cursor: Any):
             if column == "age":
                 if not user_input.isdigit() or int(user_input) < 18:
                     print("\n[!] You must be at least 18 years old to use this service!")
-                    input()
+                    sleep(2)
                     clear()
                     continue
 
@@ -178,7 +179,7 @@ def signup(cursor: Any):
             if column == "password":
                 if len(user_input) < 8:
                     print("\n[!] Password must be at least 8 characters long.")
-                    input()
+                    sleep(2)
                     clear()
                     continue
 
@@ -188,14 +189,14 @@ def signup(cursor: Any):
             if column == "email":
                 if not re.match(email_regex, user_input):
                     print("\n[!] Please enter a valid email address.")
-                    input()
+                    sleep(2)
                     clear()
                     continue
 
             # Check for non-null constraint on Fname
             if column == "fname" and not user_input:
                 print("\n[!] First name cannot be empty.")
-                input()
+                sleep(2)
                 clear()
                 continue
 
@@ -319,7 +320,201 @@ def profile(cursor: Any, user_id: int):
         """
         print(profile_head)
 
-# main runner function
+
+def create_post(cursor, user_id=1):
+    categories = {
+        0: "Education",
+        1: "Food",
+        2: "Technology",
+        3: "Animals",
+        4: "Fitness",
+        5: "Travel",
+        6: "Gaming",
+        7: "Literature",
+        8: "Art",
+    }
+
+    print("\n  Select a category:\n")
+    for key, category in categories.items():
+        if key%2 == 0:
+            print(f"{key}: {GREEN}{category}{RESET}", end="\t")
+            continue
+        print(f"{key}: {GREEN}{category}{RESET}")
+    while True:
+        key = int(input("\n\n  Enter category [0-8]: "))
+        if key not in categories:
+            print("\n  Invalid selection! Please select a category between 0 and 8.")
+            continue
+        break
+
+    # prompt for the post content
+    category = categories[key].lower()
+    content = input("\n  Post content: ")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    query = "INSERT INTO post (user_id, content, category, timestamp) VALUES (%s, %s, %s, %s)"
+    values = (user_id, content, category, timestamp)
+    cursor.execute(query, values)
+
+    print("Post created successfully!")
+    
+
+
+def see_posts(cursor, user_id):
+    reactions = {
+        1: "Terrible",
+        2: "Bad",
+        3: "Neutral",
+        4: "Good",
+        5: "Excellent"
+    }
+
+    # Query to fetch posts along with their timestamps
+    cursor.execute("SELECT post_id, content, timestamp FROM post ORDER BY timestamp DESC")
+    posts = cursor.fetchall()
+
+    for post_id, content, timestamp in posts:
+        # check if the user has already reacted to this post
+        cursor.execute("SELECT reaction_id FROM reaction WHERE post_id = %s AND user_id = %s", (post_id, user_id))
+        if cursor.fetchone():
+            # user already reacted to the post, moving on to the next one
+            continue
+
+        # display the post
+        print(f"  {GREEN}#{post_id}   {BLUE}{timestamp}{RESET}")
+        print(f"  {content}\n\n")
+
+        # get user reaction
+        while True:
+            try:
+                reaction = int(input("  What's your reaction? (1-5): "))
+                if 1 <= reaction <= 5:
+                    if reaction in [1,2]:
+                        print(f"  You reacted {RED}{reactions[reaction]}{RESET} to the post.\n")
+                    elif reaction in [4,5]:
+                        print(f"  You reacted {GREEN}{reactions[reaction]}{RESET} to the post.\n")
+                    else:
+                        print(f"  You reacted {YELLOW}{reactions[reaction]}{RESET} to the post.\n")
+                    break
+                else:
+                    print("  Invalid reaction. Please enter a number between 1 and 5.")
+            except ValueError:
+                print("  Invalid input. Please enter a number.")
+
+        # insert the reaction data
+        values = (post_id, user_id, reaction, datetime.now().strftime("%Y-%m-%d %H:%M"))
+        cursor.execute("INSERT INTO reaction (post_id, user_id, reaction_score, timestamp) VALUES (%s, %s, %s, %s)", values)
+
+    print()
+    print_centered("You have seen all the Posts!")
+
+
+def admin_see_posts(cursor):
+    # Query to fetch posts along with their average reaction scores
+    query = """
+    SELECT p.post_id, p.content, p.timestamp, AVG(r.reaction_score) as avg_score
+    FROM post p
+    LEFT JOIN reaction r ON p.post_id = r.post_id
+    GROUP BY p.post_id
+    ORDER BY p.timestamp DESC
+    """
+    cursor.execute(query)
+    posts = cursor.fetchall()
+
+    for post_id, content, timestamp, avg_score in posts:
+        # Display the post
+        print(f" {GREEN}#{post_id}   {BLUE}{timestamp}{RESET}")
+        print(f" {content}\n\n")
+        # Display the average reaction score
+        print(f" Average Reaction Score: {avg_score:.2f}\n\n")
+
+
+# DATA EXTRACTION =======================================================================
+
+def fetch_table_data(cursor, table_name):
+    cursor.execute('SELECT * FROM ' + table_name)
+    header = [row[0] for row in cursor.description]
+    rows = cursor.fetchall()
+    return header, rows
+
+
+def export_app_data(cursor, table_name):
+    # check if the folder exists else create it
+    data_folder = "Data"
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
+
+    today = datetime.now().strftime('%d%m%Y') # DDMMYYYY
+    filename = f"{table_name}-{today}.xlsx"
+
+    filepath = os.path.join(data_folder, filename)  # file path
+    
+    workbook = xlsxwriter.Workbook(filepath)
+    worksheet = workbook.add_worksheet('Sheet1')
+    header_cell_format = workbook.add_format({'bold': True, 'border': True, 'bg_color': 'yellow'})
+    body_cell_format = workbook.add_format({'border': True})
+    header, rows = fetch_table_data(cursor, table_name)
+    row_index = 0
+    column_index = 0
+    for column_name in header:
+        worksheet.write(row_index, column_index, column_name, header_cell_format)
+        column_index += 1
+    row_index += 1
+    for row in rows:
+        column_index = 0
+        for column in row:
+            worksheet.write(row_index, column_index, column, body_cell_format)
+            column_index += 1
+        row_index += 1
+    print(f' {row_index} Records successfully exported to {filepath}\n')
+    workbook.close()
+
+
+# DATA VISUALIZATION ====================================================================
+
+def fetch_category_scores(engine):
+    query = """
+    SELECT p.category, SUM(r.reaction_score) as total_score
+    FROM post p
+    JOIN reaction r ON p.post_id = r.post_id
+    GROUP BY p.category
+    ORDER BY total_score DESC
+    """
+    df = pd.read_sql_query(query, engine)
+    return df
+
+
+def export_category_scores(df):
+    data_folder = "Data"
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
+
+    today = datetime.now().strftime('%d%m%Y')  # DDMMYYYY
+
+    # Bar Graph
+    plt.figure(figsize=(10, 6))
+    plt.bar(df['category'], df['total_score'])
+    plt.xlabel('Category')
+    plt.ylabel('Total Score')
+    plt.title('Top Categories by Reaction Score')
+    plt.xticks(rotation=45)
+    # saving data to file
+    filename = f"cat-score-bar-{today}.png"
+    path = os.path.join(data_folder, filename)
+    plt.savefig(path)
+
+    # Pie Chart
+    plt.figure(figsize=(10, 6))
+    plt.pie(df['total_score'], labels=df['category'], autopct='%1.1f%%')
+    plt.title('Top Categories by Reaction Score')
+    # saving data to file
+    filename = f"cat-score-pie-{today}.png"
+    path = os.path.join(data_folder, filename)
+    plt.savefig(path)
+
+
+# MAIN CODE =============================================================================
+
 def main():
     clear()
     banner()
@@ -365,25 +560,71 @@ def main():
         choice = input("\nEnter your choice (1-3): ")
 
         if choice == "1":
-            # TODO: create a login function check if the user present or else ask to create account
-            #       -> func return true is present, else false
-            #       then display profile() module
             user = login(cursor=cursor)
             sleep(2)
             if user:
                 while True:
+                    # display profile
                     profile(cursor, user)
-                    input()
-                    break
-                # TODO: WIP
-                if user == 1:  # ADMIN user ID
-                    # ADMIN profile
-                    #  ADMIN can create post and stuff
-                    pass
-                else:
-                    # Other user profile
-                    # User can read the post according to chronological order
-                    pass
+
+                    if user == 1:  # ADMIN user ID
+                        print("\nWhat would you like to do?\n")
+                        print(f"(1) {BLUE}Create Post{RESET}")
+                        print(f"(2) {BLUE}See Posts{RESET}")  # display post along with reaction
+                        print(f"(3) {BLUE}Export Data{RESET}")
+                        print(f"(4) {RED}Sign Out{RESET}")
+                        choice = input("\nEnter your choice (1-4): ")
+                        if choice == "1":
+                            create_post(cursor=cursor, user_id=user)
+                            db_conn.commit()
+                            sleep(2)
+
+                        elif choice == "2":
+                            admin_see_posts(cursor=cursor)
+                            sleep(2)
+
+                        elif choice == "3":
+                            print(f"{GREEN}\n\n  Exporting data... Please wait...{RESET}")
+
+                            # exporting excel sheet data
+                            tables_to_export = ['account', 'user', 'post', 'reaction']
+                            for table in tables_to_export:
+                                export_app_data(cursor, table)
+
+                            # exporting graphical data
+                            engine = create_engine(f'mysql+mysqlconnector://{USER}:{PASSWD}@{HOST}/{DATABASE}')
+                            df = fetch_category_scores(engine) # SQLAlchemy engine to work with pandas
+                            export_category_scores(df)
+                            sleep(2)
+
+                        elif choice == "4":
+                            print("\n\n\n")
+                            print_centered("Signing Out... See ya later!")
+                            sleep(2)
+                            break
+                        else:
+                            print("\nInvalid choice. Please enter a number between 1-4.")
+                            sleep(2)
+                    else:
+                        # normal user
+                        # user can read the post according to chronological order
+                        print("\nWhat would you like to do?\n")
+                        print(f"(1) {BLUE}See Posts{RESET}")
+                        print(f"(2) {RED}Sign Out{RESET}")
+                        choice = input("\nEnter your choice (1-2): ")
+
+                        if choice == "1":
+                            see_posts(cursor=cursor, user_id=user)
+                            sleep(2)
+
+                        elif choice == "2":
+                            print("\n\n\n")
+                            print_centered("Signing Out... See ya later!")
+                            sleep(2)
+                            break
+                        else:
+                            print("\nInvalid choice. Please enter a number between 1-2.")
+                            sleep(2)
 
         elif choice == "2":
             new_user = signup(cursor=cursor)
@@ -397,10 +638,10 @@ def main():
                     db_conn.commit()
                     print("\nAccount has been created successfully!")
                     print_centered(f"{BLUE}Welcome to NexVerse!{RESET}")
-                    input()
+                    sleep(2)
             else:
                 print("\nPlease try again!")
-                input()
+                sleep(2)
         elif choice == "3":
             print("Exiting NexVerse... See you soon!")
             db_conn.close()
@@ -409,11 +650,9 @@ def main():
             print("\nInvalid choice. Please enter a number between 1-3.")
 
 
-
 if __name__ == "__main__":
     try:
         main()
     except sql.Error as err:
         print(f"ERROR: {err}")
         sys.exit(-1)
-
